cdb0a1d8ca7ed62868be13fab768ac9e36027e0b
solidogen <solidogen@gmail.com>
2020-12-10 16:51:00 +0100

    ViewModel factory research

--- app/src/main/java/com/example/conductormvvm/ui/features/home/HomeController.kt
+++ app/src/main/java/com/example/conductormvvm/ui/features/home/HomeController.kt
@@ -3,13 +3,14 @@ package com.example.conductormvvm.ui.features.home
 import com.example.conductormvvm.R
 import com.example.conductormvvm.ui.base.BaseController
 import org.koin.core.component.inject
+import timber.log.Timber
 
 class HomeController : BaseController(R.layout.controller_home) {
 
     private val viewModel: HomeViewModel by inject()
 
     override fun onViewCreated() {
-        viewModel.toString()
+        Timber.d(viewModel.toString())
         // todo subscribe to stateflow + sharedflow
     }
 }
